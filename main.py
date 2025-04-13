8bb7b464101449f7edb535f910b7ad3ea4b870e7
Lauren Turner <turnerlaurenn2@gmail.com>
2025-04-12 22:37:53 -0500

    Fixed window size and extra window pop-up

--- main.py
+++ main.py
@@ -9,31 +9,29 @@ class QuizApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Quiz Bowl Application")
-        self.geometry("300x150") 
+        self.geometry("400x200")
 
         self.admin_window = None
-        self.quiz_window = None  
+        self.quiz_window = None
 
         self.create_widgets()
 
     def create_widgets(self):
-        admin_button = tk.Button(self, text="Admin Interface", command=self.open_admin_panel, padx=20, pady=10)
-        admin_button.pack(pady=15)
+        quiz_button = tk.Button(self, text="Start Quiz", command=self.open_quiz_interface, padx=40, pady=20, font=("Arial", 14)) # Bigger button, larger font
+        quiz_button.pack(pady=(30, 15)) # Top padding, space below
 
-        quiz_button = tk.Button(self, text="Start Quiz", command=self.open_quiz_interface, padx=20, pady=10) 
-        quiz_button.pack(pady=15)
+        admin_button = tk.Button(self, text="Admin Interface", command=self.open_admin_panel, padx=20, pady=10)
+        admin_button.pack(pady=(15, 30)) # Space above, bottom padding
 
     def open_admin_panel(self):
         if not self.admin_window or not tk.Toplevel.winfo_exists(self.admin_window):
-            self.admin_window = tk.Toplevel(self)
-            admin_panel = AdminPanel(self.admin_window)
+            self.admin_window = AdminPanel(self)  # Pass 'self' (the main application window) as the master
         else:
             self.admin_window.lift() # Bring existing window to front
 
-    def open_quiz_interface(self):  
+    def open_quiz_interface(self):
         if not self.quiz_window or not tk.Toplevel.winfo_exists(self.quiz_window):
-            self.quiz_window = tk.Toplevel(self)
-            quiz_interface = QuizInterface(self.quiz_window)  
+            self.quiz_window = QuizInterface()  # Instantiate QuizInterface directly as a Toplevel
         else:
             self.quiz_window.lift()
 
